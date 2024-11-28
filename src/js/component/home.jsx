2c6b4c96ec48da0ab00035aad77f38a135c82d01
Gisela94 <gisela.garrido@hotmail.com>
2024-11-28 16:39:48 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [color, setColor] = useState("red");
+  const [lights, setLights] = useState([
+    { id: "red", color: "red" },
+    { id: "yellow", color: "yellow" },
+    { id: "green", color: "green" },
+  ]); 
+
+  
+  const changeColor = () => {
+    const colors = ["red", "yellow", "green"];
+    const currentIndex = colors.indexOf(color); //la función indexOf busca el ígit push al en el array colors
+    const nextColor = colors[(currentIndex + 1) % colors.length]; //currentIndex + 1: Aumenta el índice actual en 1 para pasar al siguiente color. // % = asegura que si llegas al final del array, vuelvas al principio.
+    setColor(nextColor); 
+  };
+
+  // Función para agregar o quitar una luz morada, alternadamente
+  const togglePurpleLight = () => {
+    // Verifica si ya existe la luz morada
+    const purpleExists = lights.some((light) => light.color === "purple");
+
+    if (purpleExists) {
+      setLights(lights.filter((light) => light.color !== "purple")); //Si existe la luz morada la elimina
+    } else {
+      const newLight = { id: "purple", color: "purple" }; // Si no existe, agrega una nueva luz morada
+      setLights([...lights, newLight]); //se utiliza para actualizar el estado lights
+    }
+  };
+
+
+  const handleClick = (selectedColor) => {
+    setColor(selectedColor); // Actualiza el color que debe brillar
+  };
+
+  return (
+    <>
+      <div className="stick"></div>
+      <div className="trafficLight">
+        {lights.map((light) => (
+          <div
+            key={light.id}
+            className={`light ${light.color} ${color === light.color ? "shine" : ""}`}
+            onClick={() => handleClick(light.color)}
+          ></div>
+        ))}
+      </div>
+      <div className="buttons">
+        <button className="btn btn-dark" onClick={togglePurpleLight}>
+          Add/Remove Purple
+        </button>
+        <button className="btn btn-dark" onClick={changeColor}>
+          Change Color
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
+
+
+
+
